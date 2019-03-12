5d951cbb4c4a4cd0d78eaa3d32416255e88fea49
Multillamas <38572360+Multillamas@users.noreply.github.com>
2019-03-12 14:48:46 -0400

    Update AssemblyInfo.cs

--- BLL/Properties/AssemblyInfo.cs
+++ BLL/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3644021f-a34f-48e0-a6b2-46df9711b24b")]
+[assembly: Guid("xxxxxxxxx")]
 
 // Version information for an assembly consists of the following four values:
 //
